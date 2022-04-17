b12d74a0a4e4cfa861a36d82072d818a776cee2e
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-17 23:54:55 +0600

    added checkout page & validation with toast

--- src/components/Checkout/Checkout.js
+++ src/components/Checkout/Checkout.js
@@ -1,9 +1,128 @@
 import React from 'react';
+import './Checkout.css';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
 const Checkout = () => {
+
+  const handleCheckout = (e) => {
+    e.preventDefault();
+    let email = e.target.email.value;
+    const name = e.target.name.value;
+    const contact = e.target.contact.value;
+    const address = e.target.address.value;
+
+    if(email && name && contact && address){
+      toast.success("Thank You for Booking, We'll contact you soon")
+    }
+  }
+
   return (
-    <div>
-      <h2>This is your checkout page</h2>
+    <div className="w-100 pt-5 checkout-page">
+      <h2 className="text-center text-4xl font-mono text-white">
+        Fill out this form
+      </h2>
+      <h3 className="text-center text-2xl font-mono text-white mb-4">
+        To Confirm your Booking
+      </h3>
+      <div className="login flex justify-center items-center">
+        <form onSubmit={handleCheckout} className="w-[80%] md:w-[60%] lg:w-[40%] form-container py-5 px-12 rounded-lg bg-slate-700">
+          <div className="mb-4">
+            <label
+              className="block text-gray-300 text-sm font-bold mb-2"
+              htmlFor="name"
+            >
+              Name
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
+              id="name"
+              type="text"
+              name="name"
+              placeholder="Enter your Name"
+              required
+            ></input>
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-300 text-sm font-bold mb-2"
+              htmlFor="email"
+            >
+              Email
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
+              id="email"
+              type="email"
+              name="email"
+              placeholder="Enter your Email"
+              required
+            ></input>
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-300 text-sm font-bold mb-2"
+              htmlFor="address"
+            >
+              address
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
+              id="address"
+              type="text"
+              name="address"
+              placeholder="Enter your address"
+              required
+            ></input>
+          </div>
+
+          <div className="mb-4">
+            <label
+              className="block text-gray-300 text-sm font-bold mb-2"
+              htmlFor="tel"
+            >
+              Contact No:
+            </label>
+            <input
+              className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-300 leading-tight focus:outline-none focus:shadow-outline"
+              id="tel"
+              type="tel"
+              name="contact"
+              placeholder="Enter your Phone Number"
+              required
+            ></input>
+          </div>
+
+          <div className="shift flex justify-between py-3 ">
+            <label className='text-white' htmlFor="cars">Choose Your Shift:</label>
+            <select id="shift" name="shift">
+              <option value="volvo">Morning</option>
+              <option value="saab">Afternoon</option>
+              <option value="opel">Evening</option>
+              <option value="audi">Night</option>
+            </select>
+          </div>
+
+          <div className="flex items-center justify-between">
+            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline btn btn-link w-[100%]">
+              Booking
+            </button>
+          </div>
+        </form>
+      </div>
+      <ToastContainer
+        position="top-center"
+        autoClose={5000}
+        hideProgressBar={false}
+        newestOnTop={false}
+        closeOnClick
+        rtl={false}
+        pauseOnFocusLoss
+        draggable
+        pauseOnHover
+      />
     </div>
   );
 };
--- src/components/Spinner/Spinner.js
+++ src/components/Spinner/Spinner.js
@@ -5,7 +5,7 @@ const Spinner = () => {
     <div className="flex justify-center items-center w-100 h-screen">
       <svg
         role="status"
-        class="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+        className="mr-2 w-8 h-8 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
         viewBox="0 0 100 101"
         fill="none"
         xmlns="http://www.w3.org/2000/svg"
