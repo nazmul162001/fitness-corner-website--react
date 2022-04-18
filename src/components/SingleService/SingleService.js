a01b9f729761d59e4a71e71dd71351a7f3f6a3fd
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-18 16:07:12 +0600

    update services info

--- src/components/SingleService/SingleService.js
+++ src/components/SingleService/SingleService.js
@@ -6,12 +6,12 @@ const SingleService = ({service}) => {
   const {name, price, description, img} = service;
   const navigate = useNavigate();
   return (
-    <div className='service-container rounded-md p-8'>
-      <img className='rounded-2xl' src= {img} alt="" />
+    <div className='service-container rounded-md p-8 text-left'>
+      <img className='rounded-2xl h-[250px]' src= {img} alt="" />
       <h2 className='text-2xl'> {name} </h2>
-      <p className='text-gray-500'> {description.slice(0, 100)} </p>
-      <h2 className='text-4xl font-bold'> ${price} </h2>
-      <button onClick={() => navigate('/checkout')} className='w-[100%] py-2 rounded-md text-white  bg-slate-500 my-2'>Checkout</button>
+      <p className='text-gray-500'> {description.slice(0, 200)} </p>
+      <h2 className='text-2xl font-medium font-mono py-2'>Price: ${price} </h2>
+      <button onClick={() => navigate('/checkout')} className='w-[100%] py-2 rounded-md text-black border-slate-300 border-2 hover:text-white hover:bg-slate-500 my-2'>Checkout</button>
     </div>
   );
 };
