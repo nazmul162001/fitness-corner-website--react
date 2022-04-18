026bd4587b6bbed854bdf49432aa907169db3786
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-18 23:56:25 +0600

    footer fixed

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -5,7 +5,7 @@ import logo from '../../images/logo/GoFitLogo (3).png'
 const Footer = () => {
   return (
     <footer className='px-10 mt-3 h-28 bg-black text-white flex items-center justify-between'>
-      <img src= {logo} alt="" />
+      <img className='hidden md:block' src= {logo} alt="" />
       <p>&copy;copyright 2022, all right reserve <a className='font-mono' href="/"> &copy; Nazmul</a> </p>
       <a className='go-to-top w-10 h-10 flex items-center justify-center rounded-full bg-gray-500' href="/">&#8593;</a>
     </footer>
