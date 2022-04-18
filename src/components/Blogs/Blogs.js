db0bedd3176ad7636d6caeaf7b494d94502665ff
nazmul162001 <nazmulprime162001@gmail.com>
2022-04-18 20:54:22 +0600

    complete blogs section

--- src/components/Blogs/Blogs.js
+++ src/components/Blogs/Blogs.js
@@ -3,7 +3,7 @@ import './Blogs.css';
 
 const Blogs = () => {
   return (
-    <div className='blogs-section relative'>
+    <div className='blogs-section relative pb-10'>
       <h2 className='text-center text-4xl font-mono font-bold py-3'>My Blogs</h2>
       <hr className='w-[200px] mx-auto border-b-2 border-black mb-1' />
       <hr className='w-[150px] mx-auto border-b-2 border-black mb-1' />
@@ -18,18 +18,19 @@ const Blogs = () => {
         <strong className='font-extrabold text-black'> Authorization:</strong>Authorization in a system security is the process of giving the user permission to access a specific resource or function. This term is often used interchangeably with access control or client privilege.In secure environments, authorization must always follow authentication. Users should first prove that their identities are genuine before an organizations administrators grant them access to the requested resources.
         </p>
         <h1 className="text-3xl font-mono mt-5">
-          Q2.What other service does firebase provide other than Authentication?
+          Q2.Why are you using firebase? What other options do you have to implement authentication?
+          <p className="text-xl text-gray-500 md:pl-10">
+            I using firebase because Firebase has the best documentation and has good support in case i am stuck somewhere. And also It is free for small and experimental projects and i can pay as i go if the project grows linearly. I can implement a lot of features in less time with small code.
+            There are many alternative for firebase authentication, like- MongoDB, AuthO, Okta, Passport, JSON Web Token etc.
+          </p>
+        </h1>
+        <h1 className="text-3xl font-mono mt-5">
+          Q3.What other service does firebase provide other than Authentication?
         </h1>
         <p className="text-xl text-gray-500 md:pl-10">
         Firebase ডেভেলপারদের কাজ সহজ করার জন্য বিভিন্ন ধরনের tools এবং services provide করে থাকে। তার মধ্যে অন্যতম একটি সার্ভিস হল user authentication. তবে এর বাইরেও Firebase আমাদের কে অনেক ধরনের সার্ভিস দিয়ে থাকে, যেমন- Firebase Hosting, Cloud Storage, Cloud Firestore, Firebase Realtime Database,  Google analytics, Cloud Messaging আরো অনেক কিছু।
         </p>
 
-        <h1 className="text-3xl font-mono mt-5">
-          Q3.Why are you using firebase? What other options do you have to implement authentication?
-          <p className="text-xl text-gray-500 md:pl-10">
-            -- - - -- -- -- --
-          </p>
-        </h1>
       </div>
     </div>
   );
